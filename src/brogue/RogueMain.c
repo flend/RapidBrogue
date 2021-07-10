753899628847b5acbf16dd5d055da1186f222dca
flend <flend@compsoc.net>
2021-07-10 07:39:00 +0100

    Decrease in light scales at 4x in RapidBrogue

--- src/brogue/RogueMain.c
+++ src/brogue/RogueMain.c
@@ -617,7 +617,7 @@ void startLevel(short oldLevelNumber, short stairDirection) {
     //  Prepare the new level
     rogue.minersLightRadius = (DCOLS - 1) * FP_FACTOR;
     for (i = 0; i < rogue.depthLevel; i++) {
-        rogue.minersLightRadius = rogue.minersLightRadius * 85 / 100;
+        rogue.minersLightRadius = rogue.minersLightRadius * MINERS_LIGHT_LEVEL_DECREASE_RATE * 85 / 100;
     }
     rogue.minersLightRadius += FP_FACTOR * 225 / 100;
     updateColors();
